ff1e0a2cbc1f24611a96c0721d436da0bca49905
Oliver Foyle <ofoyle@gmail.com>
2013-10-21 20:12:41 +0100

    Add fundamentals for creating and checking users

--- RSAS-server/User.cs
+++ RSAS-server/User.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using RSAS.Plugins;
 
 namespace RSAS.ServerSide
 {
     class User
     {
-        private User()
+        PluginLoader pluginLoader;
+        string username;
+
+        public string Username { get{return this.username;} }
+
+        private User(string username)
         {
-            
+            this.username = username;
         }
 
         public static User CreateFromUsername(string username)
         {
-            return new User();
+            if (Directory.Exists(Path.Combine(Settings.USERPATH, username)))
+                return new User(username);
+            else
+                return null;
         }
     }
 }
