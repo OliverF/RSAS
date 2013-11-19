37603f5a490826d10e3d27e688ee274fba34cd9b
Oliver Foyle <ofoyle@gmail.com>
2013-11-19 00:53:36 +0000

    Add LoadPlugins method

--- RSAS-common/Plugins/PluginLoader.cs
+++ RSAS-common/Plugins/PluginLoader.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace RSAS.Plugins
 {
     public class PluginLoader
     {
+        private List<Plugin> plugins = new List<Plugin>();
+
+        public void LoadPlugins(string pluginDirectory, string entryScriptName, PluginFramework framework)
+        {
+            if (Directory.Exists(pluginDirectory))
+            {
+                string[] paths = Directory.GetDirectories(pluginDirectory);
+
+                foreach (string path in paths)
+                {
+                    string entry = Path.Combine(path, entryScriptName);
+                    if (File.Exists(entry))
+                    {
+                        Plugin p = new Plugin(entry, framework);
+                        plugins.Add(p);
+                    }
+                }
+            }
+        }
     }
 }
