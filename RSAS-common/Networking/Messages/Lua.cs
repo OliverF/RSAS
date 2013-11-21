e597939dbcff01f74c4c9b984ad64273a1c3d931
Oliver Foyle <ofoyle@gmail.com>
2013-11-21 18:54:50 +0000

    Add LuaData message type

---
+++ RSAS-common/Networking/Messages/Lua.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lua4Net;
+using Lua4Net.Types;
+using RSAS.Utilities;
+
+namespace RSAS.Networking.Messages
+{
+    [Serializable]
+    public class LuaData : Message
+    {
+        Dictionary<string, LuaType> serializedTable;
+
+        public string Identifier { get; set; }
+
+        public LuaData(LuaTable table, string identifier)
+        {
+            this.serializedTable = LuaUtilities.LuaTableToDictionary(table);
+            this.Identifier = identifier;
+        }
+
+        public void GetLuaTable(LuaTable lt)
+        {
+            LuaUtilities.DictionaryToLuaTable(this.serializedTable, lt);
+        }
+    }
+}
