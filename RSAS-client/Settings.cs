7c17c285b14b5bc88793192551727028df3f93f3
Oliver Foyle <ofoyle@gmail.com>
2013-11-22 03:16:47 +0000

    Add client-side Settings class

---
+++ RSAS-client/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSAS.ClientSide
+{
+    class Settings
+    {
+        public const string ENTRYSCRIPTNAME = "cl_entry.lua";
+        public const string PLUGINPATH = "plugins";
+    }
+}
