3ebcd42c99e96e2a02677a45f6382fc8e04d2f72
Oliver Foyle <ofoyle@gmail.com>
2014-02-23 21:08:25 +0000

    Move Lua function definitions to individual methods

--- RSAS-client/GUIFramework.cs
+++ RSAS-client/GUIFramework.cs
@@ -21,224 +21,25 @@ namespace RSAS.ClientSide
 
         Dictionary<string, Control> controls = new Dictionary<string, Control>();
 
+        Control parent;
+
         public GUIFramework(Control parent)
         {
+            this.parent = parent;
+
             this.frameworkScriptNames.Add("gui.lua");
 
             this.registerEvents.Add(delegate(Lua lua)
             {
-                lua.RegisterGlobalFunction("_RSAS_GUI_CreateControl", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[1] as LuaString;
-
-                    //return if no unique control ID is specified
-                    if (controlID == null)
-                    {
-                        return;
-                    }
-
-                    ControlType type;
-                    try
-                    {
-                        type = (ControlType)Enum.Parse(typeof(ControlType), args.Input[0].ToString(), true);
-
-                        Control control = ControlFromType(type);
-
-                        controls.Add(controlID.Value, control);
-                        parent.Controls.Add(control);
-                        return;
-                    }
-                    catch (ArgumentException)
-                    {
-                        return;
-                    }
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Control_SetParent", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-                    LuaString parentControlID = args.Input[1] as LuaString;
-
-                    if (controlID == null || parentControlID == null || !this.controls.ContainsKey(controlID.Value) || !this.controls.ContainsKey(parentControlID.Value))
-                        return;
-
-                    Control parentControl = this.controls[parentControlID.Value];
-
-                    parentControl.Controls.Add(this.controls[controlID.Value]);
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Control_SetLocation", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-                    LuaNumber x = args.Input[1] as LuaNumber;
-                    LuaNumber y = args.Input[2] as LuaNumber;
-
-                    if (controlID == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    if (x != null && y != null)
-                    {
-                        controls[controlID.Value].Location = new Point((int)x.Value, (int)y.Value);
-                    }
-                    else if (x != null)
-                    {
-                        controls[controlID.Value].Location = new Point((int)x.Value, controls[controlID.Value].Location.Y);
-                    }
-                    else if (y != null)
-                    {
-                        controls[controlID.Value].Location = new Point(controls[controlID.Value].Location.X, (int)y.Value);
-                    }
-                    
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Control_SetSize", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-                    LuaNumber width = args.Input[1] as LuaNumber;
-                    LuaNumber height = args.Input[2] as LuaNumber;
-
-                    if (controlID == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    if (width != null && height != null)
-                    {
-                        controls[controlID.Value].Size = new Size((int)width.Value, (int)height.Value);
-                    }
-                    else if (width != null)
-                    {
-                        controls[controlID.Value].Size = new Size((int)width.Value, controls[controlID.Value].Size.Height);
-                    }
-                    else if (height != null)
-                    {
-                        controls[controlID.Value].Size = new Size(controls[controlID.Value].Size.Width, (int)height.Value);
-                    }
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Control_GetLocation", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-
-                    if (controlID == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    args.Output.Add(new LuaNumber(controls[controlID.Value].Location.X));
-                    args.Output.Add(new LuaNumber(controls[controlID.Value].Location.Y));
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Control_GetSize", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-
-                    if (controlID == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    args.Output.Add(new LuaNumber(controls[controlID.Value].Size.Width));
-                    args.Output.Add(new LuaNumber(controls[controlID.Value].Size.Height));
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Chart_SetXY", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-                    LuaTable values = args.Input[1] as LuaTable;
-                    LuaString seriesName = args.Input[2] as LuaString;
-
-                    if (values == null || controlID == null || seriesName == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    Chart chart = this.controls[controlID.Value] as Chart;
-
-                    if (chart == null)
-                        return;
-                    
-                    Series series = chart.Series.FindByName(seriesName.Value);
-
-                    if (series == null)
-                        return;
-
-                    //reset
-                    series.Points.Clear();
-
-                    Dictionary<LuaValueType, DataPoint> points = new Dictionary<LuaValueType, DataPoint>();
-
-                    LuaUtilities.RecurseLuaTableCallback callback = new LuaUtilities.RecurseLuaTableCallback(delegate(List<LuaValueType> path, LuaValueType key, LuaType value)
-                    {
-                        if (path.Count <= 0)
-                            return;
-
-                        //set new point if this key doesn't exist
-                        if (!points.ContainsKey(path[0]))
-                            points.Add(path[0], new DataPoint());
-
-                        LuaNumber component = value as LuaNumber;
-
-                        if (component == null)
-                            return;
-
-                        //check type of element - x or y
-                        if (key.ToString() == "x")
-                        {
-                            points[path[0]].XValue = component.Value;
-                        }
-                        else if (key.ToString() == "y")
-                        {
-                            double[] yValues = { component.Value };
-                            points[path[0]].YValues = yValues;
-                        }
-                    });
-
-                    LuaUtilities.RecurseLuaTable(values, callback);
-
-                    foreach (KeyValuePair<LuaValueType, DataPoint> kv in points)
-                        series.Points.Add(kv.Value);
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Chart_CreateSeries", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-                    LuaString seriesName = args.Input[1] as LuaString;
-                    LuaString seriesType = args.Input[2] as LuaString;
-
-                    if (controlID == null || seriesName == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    Chart chart = this.controls[controlID.Value] as Chart;
-
-                    if (chart == null)
-                        return;
-
-                    Series series = new Series(seriesName.Value);
-                    series.ChartType = SeriesChartType.Line;
-                    
-
-                    if (seriesType != null)
-                    {
-                        try
-                        {
-                            SeriesChartType type = (SeriesChartType)Enum.Parse(typeof(SeriesChartType), seriesType.ToString(), true);
-                            series.ChartType = type;
-                        }
-                        catch (ArgumentException) { }
-                    }
-
-                    chart.Series.Add(series);
-                });
-
-                lua.RegisterGlobalFunction("_RSAS_GUI_Label_SetText", delegate(LuaManagedFunctionArgs args)
-                {
-                    LuaString controlID = args.Input[0] as LuaString;
-                    LuaString text = args.Input[1] as LuaString;
-
-                    if (controlID == null || text == null || !this.controls.ContainsKey(controlID.Value))
-                        return;
-
-                    Label label = this.controls[controlID.Value] as Label;
-
-                    if (label == null)
-                        return;
-
-                    label.Text = text.Value;
-                    label.Update();
-                });
+                lua.RegisterGlobalFunction("_RSAS_GUI_CreateControl", this.CreateControl);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Control_SetParent", this.ControlSetParent);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Control_SetLocation", this.ControlSetLocation);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Control_SetSize", this.ControlSetSize);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Control_GetLocation", this.ControlGetLocation);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Control_GetSize", this.ControlGetSize);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Chart_SetXY", this.ChartSetXY);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Chart_CreateSeries", this.ChartCreateSeries);
+                lua.RegisterGlobalFunction("_RSAS_GUI_Label_SetText", this.LabelSetText);
             });
         }
 
@@ -272,6 +73,218 @@ namespace RSAS.ClientSide
                     {
                         return new Control();
                     }
+            }
+        }
+
+        private void LabelSetText(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+            LuaString text = args.Input[1] as LuaString;
+
+            if (controlID == null || text == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            Label label = this.controls[controlID.Value] as Label;
+
+            if (label == null)
+                return;
+
+            label.Text = text.Value;
+            label.Update();
+        }
+
+        private void ChartCreateSeries(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+            LuaString seriesName = args.Input[1] as LuaString;
+            LuaString seriesType = args.Input[2] as LuaString;
+
+            if (controlID == null || seriesName == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            Chart chart = this.controls[controlID.Value] as Chart;
+
+            if (chart == null)
+                return;
+
+            Series series = new Series(seriesName.Value);
+            series.ChartType = SeriesChartType.Line;
+
+
+            if (seriesType != null)
+            {
+                try
+                {
+                    SeriesChartType type = (SeriesChartType)Enum.Parse(typeof(SeriesChartType), seriesType.ToString(), true);
+                    series.ChartType = type;
+                }
+                catch (ArgumentException) { }
+            }
+
+            chart.Series.Add(series);
+        }
+
+        private void ChartSetXY(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+            LuaTable values = args.Input[1] as LuaTable;
+            LuaString seriesName = args.Input[2] as LuaString;
+
+            if (values == null || controlID == null || seriesName == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            Chart chart = this.controls[controlID.Value] as Chart;
+
+            if (chart == null)
+                return;
+
+            Series series = chart.Series.FindByName(seriesName.Value);
+
+            if (series == null)
+                return;
+
+            //reset
+            series.Points.Clear();
+
+            Dictionary<LuaValueType, DataPoint> points = new Dictionary<LuaValueType, DataPoint>();
+
+            LuaUtilities.RecurseLuaTableCallback callback = new LuaUtilities.RecurseLuaTableCallback(delegate(List<LuaValueType> path, LuaValueType key, LuaType value)
+            {
+                if (path.Count <= 0)
+                    return;
+
+                //set new point if this key doesn't exist
+                if (!points.ContainsKey(path[0]))
+                    points.Add(path[0], new DataPoint());
+
+                LuaNumber component = value as LuaNumber;
+
+                if (component == null)
+                    return;
+
+                //check type of element - x or y
+                if (key.ToString() == "x")
+                {
+                    points[path[0]].XValue = component.Value;
+                }
+                else if (key.ToString() == "y")
+                {
+                    double[] yValues = { component.Value };
+                    points[path[0]].YValues = yValues;
+                }
+            });
+
+            LuaUtilities.RecurseLuaTable(values, callback);
+
+            foreach (KeyValuePair<LuaValueType, DataPoint> kv in points)
+                series.Points.Add(kv.Value);
+        }
+
+        private void ControlGetSize(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+
+            if (controlID == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            args.Output.Add(new LuaNumber(controls[controlID.Value].Size.Width));
+            args.Output.Add(new LuaNumber(controls[controlID.Value].Size.Height));
+        }
+
+        private void ControlGetLocation(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+
+            if (controlID == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            args.Output.Add(new LuaNumber(controls[controlID.Value].Location.X));
+            args.Output.Add(new LuaNumber(controls[controlID.Value].Location.Y));
+        }
+
+        private void ControlSetSize(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+            LuaNumber width = args.Input[1] as LuaNumber;
+            LuaNumber height = args.Input[2] as LuaNumber;
+
+            if (controlID == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            if (width != null && height != null)
+            {
+                controls[controlID.Value].Size = new Size((int)width.Value, (int)height.Value);
+            }
+            else if (width != null)
+            {
+                controls[controlID.Value].Size = new Size((int)width.Value, controls[controlID.Value].Size.Height);
+            }
+            else if (height != null)
+            {
+                controls[controlID.Value].Size = new Size(controls[controlID.Value].Size.Width, (int)height.Value);
+            }
+        }
+
+        private void ControlSetLocation(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+            LuaNumber x = args.Input[1] as LuaNumber;
+            LuaNumber y = args.Input[2] as LuaNumber;
+
+            if (controlID == null || !this.controls.ContainsKey(controlID.Value))
+                return;
+
+            if (x != null && y != null)
+            {
+                controls[controlID.Value].Location = new Point((int)x.Value, (int)y.Value);
+            }
+            else if (x != null)
+            {
+                controls[controlID.Value].Location = new Point((int)x.Value, controls[controlID.Value].Location.Y);
+            }
+            else if (y != null)
+            {
+                controls[controlID.Value].Location = new Point(controls[controlID.Value].Location.X, (int)y.Value);
+            }
+        }
+
+        private void ControlSetParent(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[0] as LuaString;
+            LuaString parentControlID = args.Input[1] as LuaString;
+
+            if (controlID == null || parentControlID == null || !this.controls.ContainsKey(controlID.Value) || !this.controls.ContainsKey(parentControlID.Value))
+                return;
+
+            Control parentControl = this.controls[parentControlID.Value];
+
+            parentControl.Controls.Add(this.controls[controlID.Value]);
+        }
+
+        private void CreateControl(LuaManagedFunctionArgs args)
+        {
+            LuaString controlID = args.Input[1] as LuaString;
+
+            //return if no unique control ID is specified
+            if (controlID == null)
+            {
+                return;
+            }
+
+            ControlType type;
+            try
+            {
+                type = (ControlType)Enum.Parse(typeof(ControlType), args.Input[0].ToString(), true);
+
+                Control control = ControlFromType(type);
+
+                controls.Add(controlID.Value, control);
+                parent.Controls.Add(control);
+                return;
+            }
+            catch (ArgumentException)
+            {
+                return;
             }
         }
     }
